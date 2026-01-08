b68597d5b1e5185e6399300f72e3bc92dcd8ff13
Tanushri K <tanushrik311@gmail.com>
2026-01-08 15:00:26 +0530

    two-pointer Approach

--- Strings/palindrome.js
+++ Strings/palindrome.js
@@ -4,3 +4,18 @@ function isPalindrome(str){
 }
 console.log(isPalindrome("racecar")); // true
 console.log(isPalindrome("hello")); // false
+
+
+function twoPointerPalindrome(str) {
+    let left = 0;
+    let right = str.length - 1;
+    while (left < right) {
+        if (str[left] !== str[right]) {
+            return false;
+        }       left++;
+        right--;
+    }
+    return true;
+}
+console.log(twoPointerPalindrome("madam")); // true
+console.log(twoPointerPalindrome("world")); // false
