d0714febb494f39da1f6cfe9292048f5b6816150
Tanushri K <tanushrik311@gmail.com>
2026-01-09 18:01:54 +0530

    3rd approach

--- Strings/reverseAString.js
+++ Strings/reverseAString.js
@@ -14,3 +14,13 @@ function reverseStringIterative(str) {
 }
 console.log(reverseStringIterative("hello")); // "olleh"
 console.log(reverseStringIterative("JavaScript")); // "tpircSavaJ"
+
+function reverseStringRecursive(str) {
+    if (str === "") {
+        return "";
+    } else {
+        return reverseStringRecursive(str.substr(1)) + str.charAt(0);
+    }
+}
+console.log(reverseStringRecursive("hello")); // "olleh"
+console.log(reverseStringRecursive("JavaScript")); // "tpircSavaJ"  
