006c46c112981e9289d55db64e4e37bb6c4088dc
Tanushri K <tanushrik311@gmail.com>
2026-01-09 18:00:51 +0530

    2st approach

--- Strings/reverseAString.js
+++ Strings/reverseAString.js
@@ -4,3 +4,13 @@ function reverseString(str) {
 }
 console.log(reverseString("hello")); // "olleh"
 console.log(reverseString("JavaScript")); // "tpircSavaJ"   
+
+function reverseStringIterative(str) {
+    let reversed = '';      
+    for (let char of str) {
+        reversed = char + reversed;
+    }                               
+    return reversed;
+}
+console.log(reverseStringIterative("hello")); // "olleh"
+console.log(reverseStringIterative("JavaScript")); // "tpircSavaJ"
