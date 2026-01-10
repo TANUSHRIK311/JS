5866b28e1ab0e65a09b6d53ffa22df746c9f782d
Tanushri K <tanushrik311@gmail.com>
2026-01-10 18:15:48 +0530

    2nd-appoach

--- Strings/anagramDetection.js
+++ Strings/anagramDetection.js
@@ -6,3 +6,21 @@ function anagramDetection(str1, str2) {
 console.log(anagramDetection("listen", "silent")); // true
 console.log(anagramDetection("hello", "world")); // false
 
+function anagramUsingFrequencyCounter(str1, str2) {
+    if (str1.length !== str2.length) {
+        return false;
+    }
+    const freqCounter = {};
+    for (let char of str1) {
+        freqCounter[char] = (freqCounter[char] || 0) + 1;
+    }
+    for (let char of str2) {
+        if (!freqCounter[char]) {
+            return false;
+        }
+        freqCounter[char]--;
+    }
+    return true;
+}
+console.log(anagramUsingFrequencyCounter("triangle", "integral")); // true
+console.log(anagramUsingFrequencyCounter("apple", "pale")); // false
