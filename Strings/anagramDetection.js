36f53fcc9e3cb826df7833eb75c514ad8b0c0ba0
Tanushri K <tanushrik311@gmail.com>
2026-01-10 18:24:09 +0530

    4th-appoach

--- Strings/anagramDetection.js
+++ Strings/anagramDetection.js
@@ -5,7 +5,7 @@ function anagramDetection(str1, str2) {
 }
 console.log(anagramDetection("listen", "silent")); // true
 console.log(anagramDetection("hello", "world")); // false
-
+//--------------------------------------------------------------------------------------
 function anagramUsingFrequencyCounter(str1, str2) {
     if (str1.length !== str2.length) {
         return false;
@@ -25,6 +25,8 @@ function anagramUsingFrequencyCounter(str1, str2) {
 console.log(anagramUsingFrequencyCounter("triangle", "integral")); // true
 console.log(anagramUsingFrequencyCounter("apple", "pale")); // false
 
+//--------------------------------------------------------------------------------------
+
 function anagramUsingMap(str1, str2) {
     if (str1.length !== str2.length) {
         return false;
@@ -43,3 +45,15 @@ function anagramUsingMap(str1, str2) {
 }
 console.log(anagramUsingMap("triangle", "integral")); // true
 console.log(anagramUsingMap("apple", "pale")); // false
+
+//--------------------------------------------------------------------------------------
+
+function anagramDetectionUsingSort(str1, str2) {
+    const normalize = str =>
+        str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+    return normalize(str1) === normalize(str2);
+}
+console.log(anagramDetectionUsingSort("Dormitory", "Dirty room")); // true
+console.log(anagramDetectionUsingSort("Hello", "World")); // false
+
+//--------------------------------------------------------------------------------------
