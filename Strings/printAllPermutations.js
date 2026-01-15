9c2290e76def5734a54d3e8c0a7d88be1a4c694f
Tanushri K <tanushrik311@gmail.com>
2026-01-15 21:16:46 +0530

    3rd-Approach

--- Strings/printAllPermutations.js
+++ Strings/printAllPermutations.js
@@ -47,3 +47,28 @@ console.log(printAllPermutations("abc"));
     return Array.from(results);
 }   
  console.log(printAllPermutationsUnique("aabc"));
+
+ //---------------------------------------------------------------------
+
+ function printAllPermutationsIterative(str) {
+    const results = [];
+    const chars = str.split('').sort();
+    const n = chars.length;
+    const factorial = (num) => (num <= 1 ? 1 : num * factorial(num - 1));
+    const totalPermutations = factorial(n);
+    for (let i = 0; i < totalPermutations; i++) {
+        let temp = chars.slice();
+        let permutation = '';
+        let k = i;
+        for (let j = n; j > 0; j--) {
+            const fact = factorial(j - 1);
+            const index = Math.floor(k / fact);
+            permutation += temp[index];
+            temp.splice(index, 1);
+            k %= fact;
+        }   
+        results.push(permutation);
+    }   
+    return results;
+}
+ console.log(printAllPermutationsIterative("abc"));
