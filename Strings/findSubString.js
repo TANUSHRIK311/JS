e886a96a84bfb66dcaeaf42ddfb9a3a6bfa240a9
Tanushri K <tanushrik311@gmail.com>
2026-01-16 17:16:25 +0530

    1st-Approach

---
+++ Strings/findSubString.js
@@ -0,0 +1,29 @@
+function findSubString(mainString, subString) {
+    if (typeof mainString !== 'string' || typeof subString !== 'string') {
+        throw new Error('Both arguments must be strings.');
+    }
+    const mainLength = mainString.length;
+    const subLength = subString.length;
+    if (subLength === 0) return 0;
+    for (let i = 0; i <= mainLength - subLength; i++) {
+        let j = 0;
+        while (j < subLength && mainString[i + j] === subString[j]) {
+            j++;
+        }
+        if (j === subLength) {
+            return i;
+        }
+    }
+    return -1;
+}
+            const index = Math.floor(k / factorial(j - 1));
+            permutation += temp[index];
+            temp.splice(index, 1);
+            k %= factorial(j - 1);
+    return Array.from(results);
+}   
+ console.log(findSubString("hello world", "world")); // Output: 6
+ console.log(findSubString("hello world", "planet")); // Output: -1
+ console.log(findSubString("abcdef", "def")); // Output: 3
+
+//---------------------------------------------------------------------
