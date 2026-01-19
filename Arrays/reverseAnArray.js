f33fd1198381dd3a293c5297dd84256a86229189
Tanushri K <tanushrik311@gmail.com>
2026-01-19 16:43:07 +0530

    3rd-approach

--- Arrays/reverseAnArray.js
+++ Arrays/reverseAnArray.js
@@ -14,6 +14,8 @@ function reverseArray(arr) {
 const array = [1, 2, 3, 4, 5];
 console.log(reverseArray(array)); // Output: [5, 4, 3, 2, 1]
 
+//--------------------------------------------------------------------
+
 function reverseArrayInPlace(arr) {
     let left = 0;
     let right = arr.length - 1;
@@ -29,5 +31,15 @@ function reverseArrayInPlace(arr) {
 const arrayInPlace = [1, 2, 3, 4, 5];
 console.log(reverseArrayInPlace(arrayInPlace)); // Output: [5, 4, 3, 2, 1]
 
+//--------------------------------------------------------------------
 
-
+function reverseArrayNew(arr) {
+    const reversedArr = [];
+    for(let i = arr.length - 1; i>= 0; i--) {
+        reversedArr.push(arr[i]);
+    }   
+    return reversedArr;
+}
+// Example usage:
+const arrayNew = [1,3,5,7,9,11,13,15,17,19];
+console.log(reverseArrayNew(arrayNew)); // Output: [19,17,15,13,11,9,7,5,3,1]
