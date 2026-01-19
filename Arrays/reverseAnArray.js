e7fe98f2a55c0209b64489a0454c13f6761d4e61
Tanushri K <tanushrik311@gmail.com>
2026-01-19 16:07:01 +0530

    1st-approach

---
+++ Arrays/reverseAnArray.js
@@ -0,0 +1,15 @@
+function reverseArray(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right) {
+        // Swap elements
+        [arr[left], arr[right]] = [arr[right], arr[left]];
+        left++;
+        right--;
+    }   
+    return arr;
+}
+
+// Example usage:
+const array = [1, 2, 3, 4, 5];
+console.log(reverseArray(array)); // Output: [5, 4, 3, 2, 1]
