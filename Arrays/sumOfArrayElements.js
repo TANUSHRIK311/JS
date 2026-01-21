ef66662ddf41fc75a8c8aa72db33652eb01bf536
Tanushri K <tanushrik311@gmail.com>
2026-01-21 15:23:44 +0530

    2nd-approach

--- Arrays/sumOfArrayElements.js
+++ Arrays/sumOfArrayElements.js
@@ -10,3 +10,15 @@ const array = [1, 2, 3, 4, 5];
 console.log(sumOfArrayElements(array)); // Output: 15
 
 //--------------------------------------------------------------------
+
+function sumOfArrayElementsRecursive(arr) {
+    if (arr.length === 0) {
+        return 0;
+    }   
+    return arr[0] + sumOfArrayElementsRecursive(arr.slice(1));
+}
+// Example usage:   
+const arrayRecursive = [1, 2, 3, 4, 5];
+console.log(sumOfArrayElementsRecursive(arrayRecursive)); // Output: 15
+
+//--------------------------------------------------------------------
