bd624c995b1c958b7d42a0cfc9a2bd6daa54d678
Tanushri K <tanushrik311@gmail.com>
2026-01-24 13:38:19 +0530

    3rd-approach

--- Arrays/smallestElementInArray.js
+++ Arrays/smallestElementInArray.js
@@ -23,3 +23,18 @@ function smallestElementInArray(arr) {
 console.log(smallestElementUsingMathMin([3, 5, 7, 2, 8])); // Output: 2
 
 //--------------------------------------------------------------------
+
+function smallestElementUsingForEach(arr) {
+    if (arr.length === 0) {
+        return null; // Return null for empty array
+    }
+    let min = arr[0];
+    arr.forEach(function(element) {
+        if (element < min) {
+            min = element;
+        }
+    });
+    return min;
+}
+
+console.log(smallestElementUsingForEach([3, 5, 7, 2, 8])); // Output: 2
