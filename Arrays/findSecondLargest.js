dc3f133333d33394614f9e51e66991b632d2375e
Tanushri K <tanushrik311@gmail.com>
2026-01-26 17:58:10 +0530

    add code

--- Arrays/findSecondLargest.js
+++ Arrays/findSecondLargest.js
@@ -17,3 +17,15 @@ function findSecondLargest(arr) {
 console.log(findSecondLargest([3, 5, 7, 2, 8])); // Output: 7
 
 //--------------------------------------------------------------------
+
+function findSecondLargestUsingSort(arr) {
+    if (arr.length < 2) {
+        return null; // Not enough elements for second largest
+    }   
+    let uniqueArr = Array.from(new Set(arr)); // Remove duplicates
+    uniqueArr.sort((a, b) => b - a);
+    return (uniqueArr.length < 2) ? null : uniqueArr[1];
+}
+console.log(findSecondLargestUsingSort([3, 5, 7, 2, 8])); // Output: 7
+
+//--------------------------------------------------------------------
