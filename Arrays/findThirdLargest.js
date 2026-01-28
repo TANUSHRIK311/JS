11461326f7adf235e9a4a9d5df9f3285a933e932
Tanushri K <tanushrik311@gmail.com>
2026-01-28 10:32:19 +0530

    addcode

--- Arrays/findThirdLargest.js
+++ Arrays/findThirdLargest.js
@@ -15,3 +15,25 @@ function thirdLargestElement(arr){
 console.log(thirdLargestElement([10, 5, 20, 8, 20])); // 8
 console.log(thirdLargestElement([3, 2, 1]));         // 1
 console.log(thirdLargestElement([5, 5, 5]));         // null
+
+//------------------------------------------------------------------
+
+function thirdLargestElement(arr) {
+    let first = -Infinity, second = -Infinity, third = -Infinity;
+
+    for (let num of arr) {
+        if (num > first) {
+            third = second;
+            second = first;
+            first = num;
+        } else if (num > second && num < first) {
+            third = second;
+            second = num;
+        } else if (num > third && num < second) {
+            third = num;
+        }
+    }
+
+    return third === -Infinity ? null : third;
+}
+console.log(thirdLargestElement([10, 5, 20, 8, 20])); // 8
